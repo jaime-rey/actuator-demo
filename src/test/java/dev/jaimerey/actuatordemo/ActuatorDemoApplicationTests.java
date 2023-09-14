f934a56f8ee43c646c96e37097b92a11981e71bd
jaime-rey <jaimereycasado@gmail.com>
2023-09-14 23:41:47 +0200

    Initial commit

---
+++ src/test/java/dev/jaimerey/actuatordemo/ActuatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jaimerey.actuatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
